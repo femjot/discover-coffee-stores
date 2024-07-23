cf652fa4daf6113978be763c148c0f1257660661
femjot <reactventi@email.com>
2024-07-23 20:13:45 +0200

    prettier format

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>Coffee Connoisseur</h1>
-            <h2 className="font-inter">The best coffee shops in town</h2>
-        </main>
-    );
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Coffee Connoisseur</h1>
+      <h2 className="font-inter">The best coffee shops in town</h2>
+    </main>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,28 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-ibmplexsans)'],
-        inter: ['var(--font-inter)']
+        sans: ["var(--font-ibmplexsans)"],
+        inter: ["var(--font-inter)"],
       },
       backgroundImage: {
         gradient: "url('/static/background.png')",
       },
       colors: {
         purple: {
-          951: 'rgba(79, 70, 229, 1)',
+          951: "rgba(79, 70, 229, 1)",
         },
         gray: {
-          950: 'hsla(0, 0%, 100%, 0.7);',
-          951: 'hsla(0, 0%, 100%, 0.4);',
-          952: '#373b64',
+          950: "hsla(0, 0%, 100%, 0.7);",
+          951: "hsla(0, 0%, 100%, 0.4);",
+          952: "#373b64",
         },
       },
     },
